5b107fdb2ad20f8ee2bd35021dff9ce0ba058ea6
Osamah Bukraa <osamah.bukraa@persgroep.be>
2020-02-29 20:11:16 +0000

    Add greeter scene, will be used for creating orders later

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ require('isomorphic-fetch');
 const TELEGRAM_BOT_TOKEN = '1121412187:AAGvvk1K0he00eLI-jpbn4fdKullEnQcoPo';
 const bot = new Telegraf(process.env.BOT_TOKEN || TELEGRAM_BOT_TOKEN);
 
+const session = require('telegraf/session')
+const Stage = require('telegraf/stage')
+const Scene = require('telegraf/scenes/base')
+const { leave } = Stage
+
+
 const {getDeposits} = require('./commands/getDeposits');
 const {getBalance} = require('./commands/getBalance');
 const {submitOrder} = require('./commands/submitOrder');
@@ -27,6 +33,24 @@ bot.on('sticker', (ctx) => ctx.reply('👍'))
 
 bot.hears('hi', (ctx) => ctx.reply('Hey there'))
 
+// Greeter scene
+const greeter = new Scene('greeter')
+greeter.enter((ctx) => ctx.reply('Hi'))
+greeter.leave((ctx) => ctx.reply('Bye'))
+greeter.hears(/hi/gi, leave())
+greeter.on('message', (ctx) => ctx.reply('Send `hi`'))
+
+// Create scene manager
+const stage = new Stage()
+stage.command('cancel', leave())
+
+// Scene registration
+stage.register(greeter)
+
+bot.use(session())
+bot.use(stage.middleware())
+bot.command('greeter', (ctx) => ctx.scene.enter('greeter'))
+
 bot.command('get_deposits', async ctx => {
     const deposits = await getDeposits();
     ctx.reply(deposits)
@@ -38,7 +62,7 @@ bot.command('balance', async ctx => {
 })
 bot.command('submit_order', async ctx => {
     const order = await submitOrder();
-    ctx.reply(order)
+    ctx.reply(`All set! Your order #${order.orderId} has been created`)
 })
 
 bot.launch()
