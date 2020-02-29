c55f329f485c28bfdc45b441e35a3f216f4635ac
Makhout Nabil <makhout.nabil@gmail.com>
2020-02-29 21:09:39 +0100

    Delete unneeded functionality and simplified

--- commands/getOrdersHist.js
+++ commands/getOrdersHist.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 const HDWalletProvider = require('truffle-hdwallet-provider')
 const sw = require('starkware_crypto')
 const Web3 = require('web3')
@@ -9,8 +7,6 @@ const envVars = require('./helpers/loadFromEnvOrConfig')()
 
 
 const ethPrivKey = envVars.ETH_PRIVATE_KEY
-// NOTE: you can also generate a new key using:`
-// const starkPrivKey = dvf.stark.createPrivateKey()
 const starkPrivKey = ethPrivKey
 const infuraURL = `https://ropsten.infura.io/v3/${envVars.INFURA_PROJECT_ID}`
 
@@ -18,26 +14,19 @@ const provider = new HDWalletProvider(ethPrivKey, infuraURL)
 const web3 = new Web3(provider)
 
 const dvfConfig = {
-  // Using dev API.
   api: 'https://api.deversifi.dev'
 }
 
 
-;(async () => {
+const getOrdersHistory = async () => {
   const dvf = await DVF(web3, dvfConfig)
 
-  const getUserConfigResponse = await dvf.getUserConfig()
+  const getOrdersHistResponse = await dvf.getOrdersHist()
 
-  console.log('getUserConfig response ->', getUserConfigResponse)
+  console.log('getOrdersHist response ->', getOrdersHistResponse)
+  return getOrdersHistResponse;
+}
 
-})()
-// Stop provider to allow process to exit.
-.then(() => {
-  console.log('Stopping provider...')
-  provider.engine.stop()
-})
-.catch(error => {
-  console.error(error)
-  process.exit(1)
-})
-
+module.exports = {
+  getOrdersHistory
+}
